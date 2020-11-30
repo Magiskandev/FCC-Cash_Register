781009d1e82b54bd3a51b3558afd1222a2434ade
Magiskandev <hello@mail.com>
2020-11-29 23:39:29 -0500

    FCC Cash Register.

--- scripts/main.js
+++ scripts/main.js
@@ -23,6 +23,7 @@ Five Dollars	$5 (FIVE)
 Ten Dollars	$10 (TEN)
 Twenty Dollars	$20 (TWENTY)
 One-hundred Dollars	$100 (ONE HUNDRED)
+
 See below for an example of a cash-in-drawer array:
 
 [
@@ -38,3 +39,63 @@ See below for an example of a cash-in-drawer array:
 ]
 */
 
+function checkCashRegister(price, cash, cid) {
+
+  let dev = [];
+  let values = [100,20,10,5,1,0.25,0.1,0.05,0.01];
+  let names = ['ONE HUNDRED', 'TWENTY', 'TEN', 'FIVE', 'ONE','QUARTER','DIME','NICKEL','PENNY'];
+  let totalIn = 0;
+  let devDada = 0;
+
+  cid.forEach(arg => totalIn += arg[1]);
+
+  let change = cash - price;
+
+  console.log(`devuelta original: ${change}.`);
+
+  const obj = {
+    status: "OPEN",
+    change: []
+  }
+  
+  console.log(totalIn);
+  
+  change == totalIn ? obj.status = "CLOSED":
+  obj.status = "INSUFFICIENT_FUNDS";
+
+  if (obj.status === "CLOSED"){
+    dev = cid;
+  }
+
+  else {
+    for (let i = 0; i < values.length; i++){
+      let enCaja = cid[values.length-(i+1)][1]
+
+      if ((change/values[i]).toFixed(2) >= 1){
+        if((change/values[i])*values[i] <= enCaja){
+          dev.push([names[i],(Math.floor(change/values[i]))*values[i]])
+          change = (change - ((Math.floor(change/values[i]))*values[i])).toFixed(2)
+
+        }else{
+          dev.push([names[i],enCaja])
+          change = (change - enCaja).toFixed(2)
+
+        }
+      }
+    }
+  }
+
+  dev.forEach(num => devDada += num[1])
+  if (devDada < (cash - price)){
+     obj.status = "INSUFFICIENT_FUNDS"
+     dev = []
+   }
+Object.assign(obj.change,dev)
+console.log(obj)
+return obj;
+
+}
+
+checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) // {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}
+
+checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) // {status: "INSUFFICIENT_FUNDS", change: []}.
